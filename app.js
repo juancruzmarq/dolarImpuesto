959d47e2d588a4a70203b880125afb62e3b96b88
Juan Cruz Marquez <marquezjuanchy@hotmail.com>
2021-09-18 20:29:42 -0300

    ejs

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000;
 
 
 //Servir contenido estatico
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
         res.render('index');
