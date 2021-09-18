eac41edf43d7a8584ddd3394b351a5f3a3b45e57
Juan Cruz Marquez <marquezjuanchy@hotmail.com>
2021-09-18 20:24:51 -0300

    errores

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 
 const express = require('express')
 const app = express()
-
+const port = 8000
 
 
 //Servir contenido estatico
-app.use(express.static('public'));
-
+app.use(express.static(__dirname + 'public'));
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-        res.render('index')
-   })
-
-
+        res.render('index');
+   });
 
 
 
  
-
-app.listen(8000)
+   app.listen(port, function() {
+        console.log('Our app is running on http://localhost:' + port);
+    });
