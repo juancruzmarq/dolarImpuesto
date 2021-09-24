88516b6593484ccabd1e2734f7572f2356b2545b
Juan Cruz Marquez <marquezjuanchy@hotmail.com>
2021-09-24 01:10:53 -0300

    opcion euro, cambios style

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,7 @@ const retencion = document.getElementById('txtRetencion');
 const total = document.getElementById('txtTotal');
 const btnCalcular = document.getElementById('btnCalcular');
 
+
 const api = 'https://api.bluelytics.com.ar/v2/latest'
 
 
@@ -15,14 +16,29 @@ async function getApi(){
     const response = await fetch(api);
     const data = await response.json();
     const dolarOficial =  data.oficial.value_sell
+    const euroOficial = data.oficial_euro.value_sell
+    console.log(euroOficial);
+    var dolarChecked = true
+
     btnCalcular.onclick = function(){
-        const precioPesosNuevo = monto.value * dolarOficial
-        precioPesos.innerText = "$" + precioPesosNuevo.toFixed(2)
-        const impuestoPaisNuevo = precioPesosNuevo.toFixed(2) * 0.30
-        impuestoPais.innerText = "$" + impuestoPaisNuevo.toFixed(2)
-        const retencionNuevo = precioPesosNuevo.toFixed(2) * 0.35
-        retencion.innerText = "$" + retencionNuevo.toFixed(2)
-        total.innerText = "$" + (precioPesosNuevo + impuestoPaisNuevo + retencionNuevo) 
+        if(dolarChecked === true){
+            var precioPesosNuevo = monto.value * dolarOficial
+            precioPesos.innerText = "$" + precioPesosNuevo.toFixed(2)
+            var impuestoPaisNuevo = precioPesosNuevo.toFixed(2) * 0.30
+            impuestoPais.innerText = "$" + impuestoPaisNuevo.toFixed(2)
+            var retencionNuevo = precioPesosNuevo.toFixed(2) * 0.35
+            retencion.innerText = "$" + retencionNuevo.toFixed(2)
+            total.innerText = "$" + (precioPesosNuevo + impuestoPaisNuevo + retencionNuevo).toFixed(2)
+        }else{
+            var precioPesosNuevo = numberWithCommas(monto.value * euroOficial)
+            precioPesos.innerText = "$" + numberWithCommas(precioPesosNuevo.toFixed(2))
+            var impuestoPaisNuevo = numberWithCommas(precioPesosNuevo.toFixed(2) * 0.30)
+            impuestoPais.innerText = "$" + numberWithCommas(impuestoPaisNuevo.toFixed(2))
+            var retencionNuevo = numberWithCommas(precioPesosNuevo.toFixed(2) * 0.35)
+            retencion.innerText = "$" + numberWithCommas(retencionNuevo.toFixed(2))
+            total.innerText = "$" + numberWithCommas((precioPesosNuevo + impuestoPaisNuevo + retencionNuevo))
+        }
+        
     }
 
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
@@ -48,7 +64,50 @@ async function getApi(){
             localStorage.setItem('theme', 'light'); //add this
         }    
     }
+    
+    var iconoMonedaEuro = document.getElementById('iconoMonedaEuro');
+    var spanMoneda = document.getElementById('spanMoneda')
+    var btnIconoMoneda = document.getElementById('btnIconoMoneda')
+    var ingreseMonto = document.getElementById('ingreseMonto')
 
+    btnIconoMoneda.onclick = function(){
+        if (iconoMonedaEuro.style.display === 'none' ){
+            monedaEuro()
+        }else{
+           monedaDolar()
+        }
+    }
+    
+
+}
+
+function resetCalculo(){
+    precioPesos.innerText = '$0.00'
+    impuestoPais.innerText ='$0.00'
+    retencion.innerText = '$0.00'
+    total.innerText ='$0.00'
+}
+
+function monedaDolar(){
+    iconoMonedaEuro.style.display = 'none';
+    iconoMonedaDolar.style.display = '';
+    spanMoneda.innerText = 'Dolar'
+    ingreseMonto.innerText = 'Ingrese monto Dolar'
+    dolarChecked = true
+    resetCalculo()
+}
+
+function monedaEuro(){
+    iconoMonedaDolar.style.display = 'none';
+    iconoMonedaEuro.style.display = '';
+    spanMoneda.innerText = 'Euro '
+    ingreseMonto.innerText = 'Ingrese monto Euro'
+    dolarChecked = false
+    resetCalculo()
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 getApi(api);
